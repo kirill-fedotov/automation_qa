5ab6be09fe5e8f46e850ce3c6cd1f9e515745b3e
DESKTOP-1VFRKBR\XXX <fedotov.k8383@gmail.com>
2022-12-13 12:08:29 +0200

    added classes: - TestDroppablePage - TestDragabblePage - DroppablePage - DragabblePage - DroppablePageLocators - DragabblePageLocators
    added tests:
    - test_simple_droppable
    - test_accept_droppable
    - test_prevent_propogation_droppable
    - test_revert_draggable_droppable
    - test_simple_dragabble
    - test_axis_restricted_dragabble

--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,4 +1,4 @@
-from pages.interactions_page import SortablePage, SelectablePage, ResizablePage
+from pages.interactions_page import SortablePage, SelectablePage, ResizablePage, DroppablePage, DragabblePage
 
 
 class TestInteractions:
@@ -32,3 +32,57 @@ class TestInteractions:
             assert max_box == ('250px', '270px'), 'Size not equal to "250px", "270px"'
             assert min_box == ('150px', '150px'), 'Size not equal to "150px", "150px"'
             assert max_resize != min_resize, 'Resizable hax not been changed'
+
+    class TestDroppablePage:
+
+        def test_simple_droppable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            text = droppable_page.drop_simple()
+            assert text == 'Dropped!', 'The elements has not been dropped'
+
+        def test_accept_droppable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            not_accept, accept = droppable_page.drop_accept()
+            assert not_accept == 'Drop here', 'The dropped element has been accepted'
+            assert accept == 'Dropped!', 'The dropped element has not been accepted'
+
+        def test_prevent_propogation_droppable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            not_greedy, not_greedy_inner, greedy, greedy_inner = droppable_page.drop_prevent_propogation()
+            assert not_greedy == 'Dropped!', 'The elements texts has not been changed'
+            assert not_greedy_inner == 'Dropped!', 'The elements texts has not been changed'
+            assert greedy == 'Outer droppable', 'The elements texts has been changed'
+            assert greedy_inner == 'Dropped!', 'The elements texts has not been changed'
+
+        def test_revert_draggable_droppable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            will_after_move, will_after_revert = droppable_page.drop_will_revert_draggable()
+            not_will_after_move, not_will_after_revert = droppable_page.drop_not_will_revert_draggable()
+            assert will_after_move != will_after_revert, 'The elements has not reverted'
+            assert not_will_after_move == not_will_after_revert, 'The elements has reverted'
+
+    class TestDragabblePage:
+
+        def test_simple_dragabble(self, driver):
+            dragabble_page = DragabblePage(driver, 'https://demoqa.com/dragabble')
+            dragabble_page.open()
+            before, after = dragabble_page.simple_drag_box()
+            assert before != after, 'The position of the box has not been changed'
+
+        def test_axis_restricted_dragabble(self, driver):
+            dragabble_page = DragabblePage(driver, 'https://demoqa.com/dragabble')
+            dragabble_page.open()
+            top_x, left_x = dragabble_page.axis_restricted_x()
+            top_y, left_y = dragabble_page.axis_restricted_y()
+            assert top_x[0][0] == top_x[1][0] and int(top_x[1][0]) == 0, \
+                'Box position has not changed or there has been a shift in the y-axis'
+            assert left_x[0][0] != left_x[1][0] and int(left_x[1][0]) != 0, \
+                'Box position has not changed or there has been a shift in the y-axis'
+            assert top_y[0][0] != top_y[1][0] and int(top_y[1][0]) != 0, \
+                'Box position has not changed or there has been a shift in the x-axis'
+            assert left_y[0][0] == left_y[1][0] and int(left_y[1][0]) == 0, \
+                'Box position has not changed or there has been a shift in the x-axis'
