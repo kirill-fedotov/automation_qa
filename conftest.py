d4807301b788e1c09c5229a8b7cd51cb96d93c11
DESKTOP-1VFRKBR\XXX <fedotov.k8383@gmail.com>
2022-12-17 17:39:18 +0200

    updated: - conftest.py

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+import allure
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,7 +9,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="function")
 def driver():
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 5.1; rv:7.0.1) Gecko/20100101 Firefox/7.0.1'})
+    driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+        "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'})
     driver.maximize_window()
     yield driver
+    attach = driver.get_screenshot_as_png()
+    allure.attach(attach, name=f'Screenshot {datetime.today()}', attachment_type=allure.attachment_type.PNG)
     driver.quit()
