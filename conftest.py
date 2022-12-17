716c8e1a6e09631f022aa72a9d4ed5a8bef62597
DESKTOP-1VFRKBR\XXX <fedotov.k8383@gmail.com>
2022-12-17 17:41:13 +0200

    updated: - conftest.py

--- conftest.py
+++ conftest.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+import allure
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,9 +9,10 @@ from fake_useragent import UserAgent
 @pytest.fixture(scope="function")
 def driver():
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    #ua = UserAgent()
     driver.execute_cdp_cmd('Network.setUserAgentOverride', {
         "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'})
     driver.maximize_window()
     yield driver
+    attach = driver.get_screenshot_as_png()
+    allure.attach(attach, name=f'Screenshot {datetime.today()}', attachment_type=allure.attachment_type.PNG)
     driver.quit()
